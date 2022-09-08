d89a521ae704865b9067e84871c9050b75d62435
AlicjaBobko <alicja_bobko@o2.pl>
2022-09-08 11:32:04 +0200

    styling: hero card component

---
+++ src/components/card-thank-you.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ThankYouPage = (props: Props) => {
+  return <div>card-thank-you</div>;
+};
+
+export default ThankYouPage;
---
+++ src/components/hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type Props = {};
+
+export const Hero = (props: Props) => {
+  return (
+    <div className="card-hero">
+      <div id="card-front">
+        <span id="card-number">0000 0000 0000 0000</span>
+        <div id="card-personal-data">
+          <span>Jane Appleseed</span>
+          <span>00/00</span>
+        </div>
+      </div>
+      <div id="card-back">
+        <span>000</span>
+      </div>
+    </div>
+  );
+};
