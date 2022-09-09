cb465c3401a8fdb6197f94884c1921ae030f3f0d
AlicjaBobko <alicja_bobko@o2.pl>
2022-09-09 11:19:03 +0200

    styling: form - bouder input gradient on hover

--- src/components/card-form.tsx
+++ src/components/card-form.tsx
@@ -1,22 +1,83 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import "./card-form.scss";
 
-interface IFormInput {
-  firstName: string;
-  lastName: string;
-  age: number;
+interface FormInput {
+  nameSurname: string;
+  cardNumber: string;
+  month: number;
+  year: number;
+  cvc: number;
 }
 
 export const CardForm = () => {
-  const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const { register, handleSubmit } = useForm<FormInput>();
+  const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
 
   return (
     <form className="card-form" onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true, maxLength: 20 })} />
-      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-      <input type="number" {...register("age", { min: 18, max: 99 })} />
-      <input type="submit" />
+      <div>
+        <label htmlFor="name">Cardholder name</label>
+        <div className="wrapper--rounded">
+          <input
+            placeholder="e.g. Jane Appleseed"
+            {...register("nameSurname", {
+              pattern: /^[a-z ,.'-]+$/,
+              required: true,
+            })}
+          />
+        </div>
+      </div>
+      <div>
+        <label htmlFor="card-number">card number</label>
+        <div className="wrapper--rounded">
+          <input
+            type="text"
+            placeholder="e.g. 1234 5678 9123 0000"
+            {...register("cardNumber", {
+              pattern: /(\d{4}[\s|-]?\d{4}[\s|-]?\d{4}[\s|-]?\d{4})/,
+              required: true,
+            })}
+          />
+        </div>
+      </div>
+      <div id="mmyy-cvc">
+        <div id="mmyy-cvc-labels">
+          <label htmlFor="mm-yy">exp. date (mm/yy)</label>
+          <label htmlFor="cvc">cvc</label>
+        </div>
+        <div id="mmyy-cvc-inputs">
+          <div className="wrapper--rounded">
+            <input
+              placeholder="MM"
+              className="mm-yy"
+              type="number"
+              {...register("month", { min: 1, max: 12, required: true })}
+            />
+          </div>
+          <div className="wrapper--rounded">
+            <input
+              placeholder="YY"
+              className="mm-yy"
+              type="number"
+              {...register("year", {
+                min: new Date().getFullYear(),
+                required: true,
+              })}
+            />
+          </div>
+          <div className="wrapper--rounded">
+            <input
+              placeholder="e.g. 012"
+              className="cvc"
+              type="number"
+              {...register("cvc", { pattern: /\d{3}/, required: true })}
+            />
+          </div>
+        </div>
+      </div>
+      <button type="submit">Confirm</button>
+      {/* <input type="submit" /> */}
     </form>
   );
 };
